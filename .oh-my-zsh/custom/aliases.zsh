58cc7d3ca57cea80efa84130a924ad4ea8b8f3fe
ivanfoo <sanfru.ivan@gmail.com>
2015-10-27 15:39:32 +0100

    Added zsh custom stuff

---
+++ .oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,33 @@
+# Making my life easier
+
+# Dirs aliases
+pro=~/projects
+gopro=~/go/src/github.com/ivanfoo
+omz=~/.oh-my-zsh
+
+# tgz shortcuts
+alias tczf='tar czfv $1 $2'
+alias txzf='tar xzfv $1' 
+
+# Fuck off stuff
+alias untmp='rm -rf /tmp/*'
+alias undown='rm -rf ~/Downloads/*'
+
+# Misc
+alias gri='grep -i'
+alias tf='tail -f'
+alias subl='sublime_text &> /dev/null &'
+alias shut='sudo shutdown -h now'
+alias jostit='sudo vim /etc/hosts'
+
+# Editing and reloading aliases
+alias cheat='vi ~/.oh-my-zsh/custom/aliases.zsh'
+alias docheat='source ~/.oh-my-zsh/custom/aliases.zsh'
+
+# Editing .vimrc
+alias vimrc='vi ~/.vimrc'
+
+# Editing and reloading zsh config
+alias cfg='vi ~/.zshrc'
+alias reload='source ~/.zshrc' 
+
